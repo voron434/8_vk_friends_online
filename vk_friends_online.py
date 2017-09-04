c392491f9f626e8c81b05769b63c480bdb66da0c
Ilya Osipov <070809010@mail.ru>
2017-09-04 18:59:25 +0300

    Use getOnline instead of get

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -7,27 +7,35 @@ from getpass import getpass
 APP_ID = os.environ.get('APP_ID')
 
 
-def fetch_friends(login, password):
-    try:
-        session = vk.AuthSession(
-            app_id=APP_ID,
-            user_login=login,
-            user_password=password,
-        )
-        vk_api = vk.API(session)
-    except vk.exceptions.VkAuthError:
-        return {'error': {'error_msg': 'Incorrect login or password'}}  # because other vk exceptions look like this
-    friends_response = vk_api.friends.get(fields='id')
-    return friends_response
+def if_error_sys_exit(response):
+    if 'error' in response:
+        print(response['error']['error_msg'])
+        raise SystemExit
 
 
 if __name__ == '__main__':
     login = input('Login:')
     password = getpass('Password (would be invisible):')
-    friends_response = fetch_friends(login, password)
-    if 'error' in friends_response:
-        print(friends_response['error']['error_msg'])
+    try:
+        session = vk.AuthSession(
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='friends'
+        )
+        vk_api = vk.API(session)
+    except vk.exceptions.VkAuthError:
+        print('Incorrect login or password')
         raise SystemExit
-    online_friends = [friend for friend in friends_response if friend['online']]
-    for friend in online_friends:
+    online_friends_ids = vk_api.friends.getOnline(online_mobile=1)
+    if_error_sys_exit(online_friends_ids)
+    online_friends_pc_info = vk_api.users.get(user_ids=online_friends_ids['online'])
+    online_friends_mobile_info = vk_api.users.get(user_ids=online_friends_ids['online_mobile'])
+    if_error_sys_exit(online_friends_pc_info)
+    if_error_sys_exit(online_friends_mobile_info)
+    print('Your pc-online friends:')
+    for friend in online_friends_pc_info:
+        print(friend['first_name'], friend['last_name'])
+    print('Your mobile-online friends:')
+    for friend in online_friends_mobile_info:
         print(friend['first_name'], friend['last_name'])
